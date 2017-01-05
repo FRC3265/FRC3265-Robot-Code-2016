6ce3f5b51e77abc2c1fac976ccc161d46a580b6f
Kylerast <Kyle.a.rast@gmail.com>
2017-01-05 15:40:00 -0500

    Add files via upload
    Robot ode 2016 with vision

--- Robot.java
+++ Robot.java
@@ -38,24 +38,33 @@ public class Robot extends IterativeRobot {
 	Talon talon4;
 	DigitalInput limitswitch;
 	Timer timer;
-	// CameraServer camera;
 	boolean enabled;
 
 	
 	public void robotInit() {
+	//Runs Once to start(initilize)robot
+		 
+		//Each talon is assigned a name (talon0 for example ) 
 		talon0 = new Talon(0);
 		talon1 = new Talon(1);
 		talon2 = new Talon(2);
 		talon3 = new Talon(3);
 		talon4 = new Talon(4);
+		
+		//Sets talon 1 & 2 to be the robots drive, This is called the drive train
 		tankDrive = new RobotDrive(talon1, talon2);
+		
+		//Sets the joystick name to joysticks
 		joystick = new Joystick(0);
+		
+		//Sets the limitswitch to limitswitch
 		limitswitch = new DigitalInput(0);
+		
+		//Sets camera to run
 		CameraServer server;
 		server = CameraServer.getInstance();
 		server.setQuality(50);
 		server.startAutomaticCapture("cam0");
-
 	}
 
 	/**
@@ -63,9 +72,13 @@ public class Robot extends IterativeRobot {
 	 */
 	
 	public void autonomousPeriodic() {
+		//Periodic is set so this code runs periodically 
 		int count = 0;
+		// Starting the timer at 0
 
 		while (isAutonomous() && isEnabled()) {
+			//This code does the same thing as Init (autonomousInit) meaning it runs constantly so long as this mode is active 
+			
 			// Set arms down
 			if (count < 900) {
 				//talon3.set(-0.70);
@@ -87,8 +100,7 @@ public class Robot extends IterativeRobot {
 			} else if (count == 4000) {
 				Timer.delay(5);
 				// Once 3500 is met, robot will stop driving until teleop is enabled.
-				// talon1.set(0.0);
-				// talon2.set(0.0);
+
 			}
 
 		}
@@ -99,8 +111,7 @@ public class Robot extends IterativeRobot {
 	 * This function is called periodically during operator control
 	 */
 	 public void teleopPeriodic() {
-	//public void teleopInit() {
-
+		 	// Code is same as autonomous except drivable
 		int counter = 0;
 		while (isOperatorControl() && isEnabled()) {
 	/**-----------------------------------------------------------------------------------------------------**/
@@ -114,93 +125,66 @@ public class Robot extends IterativeRobot {
 			boolean yButton = joystick.getRawButton(4);
 			boolean xButton = joystick.getRawButton(3);
 	/**-----------------------------------------------------------------------------------------------------**/
+		tankDrive.tankDrive(rightYAxis *.5 , leftYAxis* .5);
 			
-			// Enables drive at 50% speed
-			tankDrive.tankDrive(rightYAxis, leftYAxis);
-			// Right bumper sets speed to 100%
-			if (rightBumper == true) {
-				tankDrive.tankDrive(rightYAxis / 1.5, leftYAxis / 1.5);
-
-			} else if (rightBumper == false) {
-
-				// Left bumper sets speed to 25%
-				if (leftBumper == true) {
-					tankDrive.tankDrive(rightYAxis * 2, leftYAxis * 2);
-				} else {
-					tankDrive.tankDrive(rightYAxis, leftYAxis);
-				}
+		//Sets ball intake 
+		if(leftBumper){
+	        talon0.set(1); 	
+		}else if(rightBumper){
+	        talon0.set(-1); 	
+		} else{
+			talon0.set(0);
+		}
+		
+		 //Raises arm with Lefttrigger
+		if (leftTrigger != 0) {
+			talon4.set(leftTrigger);
+			talon3.set(leftTrigger);
 			}
-
-	        	/*talon0.set(-1); 	*/
-
-			
-			if(limitswitch.get()) {	
-	        	talon0.set(0); 	
-	        
-	        
-	 		} else if(bButton == true ) {
-	 	        	talon0.set(-1);
-	 	        }
-
-			// else {
-			// talon0.set(0);
-			// }
-
-			// Makes start button toggle and sets ballIn to start
-
-			// ----------------------------------------------------------------------
-
-			// ----------------------------------------------------------------------
-
-			// Raises arm with leftTrigger
-			if (leftTrigger != 0) {
-				talon4.set(leftTrigger);
-				talon3.set(leftTrigger);
-			}
-
+		
 			// Lowers arm with rightTrigger
 			else if (rightTrigger != 0) {
-
-				// Trying to lower speed which arm goes down with
-
 				talon4.set(-rightTrigger);
 				talon3.set(-rightTrigger);
 				talon4.disable();
 				talon3.disable();
-
-				/*
-				 * Try seting a positive value of .05 or somthing close instead
-				 * of disabling it
-				 */
-
+			}else{
+				talon4.disable();
+				talon3.disable();
 			}
-		}
-
-		/** robot code here! **/
-		//Timer.delay(0.005);
+		}	
 	}
-
+	 
 	/**
 	 * This function is called periodically during test mode
 	 */
 	public void testPeriodic() {
+		
+		while (isTest() && isEnabled()) {
+			double leftYAxis = joystick.getRawAxis(1);
+			double rightYAxis = joystick.getRawAxis(5);
+			double leftTrigger = joystick.getRawAxis(2);
+			double rightTrigger = joystick.getRawAxis(3);
+			boolean rightBumper = joystick.getRawButton(6);
+			boolean leftBumper = joystick.getRawButton(5);
+			boolean bButton = joystick.getRawButton(2);
+			boolean yButton = joystick.getRawButton(4);
+			boolean xButton = joystick.getRawButton(3);
+			
+			tankDrive.tankDrive(rightYAxis, leftYAxis);
+			
+			if(leftBumper){
+	        	talon0.set(1); 	
 
-		while (isTest()) {
+			}else if(rightBumper){
+	        	talon0.set(-1); 	
 
-			talon0.set(0.4);
-			Timer.delay(2);
-			talon1.set(0.4);
-			Timer.delay(2);
-			talon2.set(0.4);
-			Timer.delay(2);
-			talon3.set(0.4);
-			Timer.delay(2);
-			talon4.set(0.4);
-			Timer.delay(2);
-			tankDrive.tankDrive(0, 0);
-			Timer.delay(1);
-
-		}
+			} else{
+				talon0.set(0);
+			}
+			}
+			
+		
 
 	}
 
